1d077a286b042dadcea06690a4d7a59f916cbfd4
DEV-PIERRE\Pierre <pierre.cangemi@solpay.com>
2018-09-10 18:01:35 +0200

    test #K7

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ hello
 
 k7
 
-dsfdfsdfbdfg fd g gfd gfdg dfg  dfgfdg fdg  ghffhfgh fds fdsf sdvsc dsf sdf edsfxdcvdsqf dsf dfs gfdg rdg 
+dsfdfsdfbdfg fd g gfd gfdg dfg  dfgfdg fdg  ghffhfgh fds fdsf sdvsc dsf sdf edsfxdcvdsqf dsf dfs gfdg rdg sdq fsdqf dsf qs
